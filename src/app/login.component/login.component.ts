4334fe8206ef5f9715955a445933a7a7ada1430c
tamilmanimurugan <tamilmani.murugan@infinire.com>
2026-05-22 14:07:56 +0530

    devlops

--- src/app/login.component/login.component.ts
+++ src/app/login.component/login.component.ts
@@ -1,50 +1,46 @@
 import { Component } from '@angular/core';
-
 import { FormsModule } from '@angular/forms';
-
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-
-  imports: [
-    FormsModule
-  ],
-
+  imports: [FormsModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-
 export class LoginComponent {
 
-  username = '';
-
-  password = '';
+  username='';
+  password='';
 
   constructor(
     private router: Router
-  ) {}
+  ){}
 
-  login() {
+  login(){
 
-    if (
-      this.username === 'admin' &&
-      this.password === '1234'
-    ) {
+    if(
+      this.username==="admin@gmail.com" &&
+      this.password==="1234"
+    ){
 
       localStorage.setItem(
         'token',
         'employee-token'
       );
 
-      this.router.navigate(['/dashboard']);
+      this.router.navigate(
+        ['/dashboard']
+      );
 
     }
 
-    else {
+    else{
 
-      alert('Invalid Username or Password');
+      alert(
+        "Invalid Email or Password"
+      );
 
     }
 
